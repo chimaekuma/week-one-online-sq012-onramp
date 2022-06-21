df9898f7d12a550f269e8ed4e0db7ed9e01876bc
Chima Samuel <chimaekuma@yahoo.com>
2022-06-21 21:13:20 +0100

    first update

---
+++ challenge-1.js
@@ -0,0 +1,7 @@
+function secondLargest(arr) {
+  let sortedArr=arr.sort(function (a, b) {return b-a})
+  return sortedArr[1];
+}
+module.exports = secondLargest
+
+console.log(secondLargest([2,5,8,10,7]))
