4b6e7a7d500db05baa1af8cdb419752a4c26cc8d
Chima Samuel <chimaekuma@yahoo.com>
2022-06-21 23:04:42 +0100

    first update

--- challenge-2.js
+++ challenge-2.js
@@ -1,5 +1,7 @@
 function filterArray(arr) {
- 
+    return arr.filter((elem)=> {return typeof elem==='number'})
+     
 }
 
 module.exports = filterArray
+console.log(filterArray([7,3,20,4,2, "you", "5"]))
